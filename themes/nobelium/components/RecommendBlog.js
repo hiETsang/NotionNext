ba986fd2adb8ffce5b2f5b7cc0210fd2c4cac580
canoe <canoe_likecode@163.com>
2024-03-18 18:54:22 +0800

    推荐文章展示优化

--- themes/nobelium/components/RecommendBlog.js
+++ themes/nobelium/components/RecommendBlog.js
@@ -5,31 +5,23 @@ import { checkContainHttp, sliceUrlFromHttp } from '@/lib/utils'
 import NotionIcon from '@/components/NotionIcon'
 import NotionPage from '@/components/NotionPage'
 
-const BlogPost = ({ post }) => {
+const RecommendBlog = ({ post }) => {
   const url = checkContainHttp(post.slug) ? sliceUrlFromHttp(post.slug) : `${siteConfig('SUB_PATH', '')}/${post.slug}`
 
   const showPreview = siteConfig('POST_LIST_PREVIEW') && post.blockMap
 
   return (
     (<Link href={url}>
-      <article key={post.id} className="mb-6 md:mb-9 flex flex-row space-x-3 md:space-x-5">
-        {/* <div className="flex-none w-[100px] h-[100px] md:w-[120px] md:h-[120px] ">
-          <LazyImage src={post?.pageCoverThumbnail} alt={post?.title} className='w-full h-full object-cover hover:brightness-75 transition-all duration-300' />
-        </div> */}
+      <article key={post.id} className="mb-3 md:mb-5 flex flex-row space-x-3 md:space-x-5">
         <div className="flex flex-col">
-          <header className="flex flex-col justify-between space-y-2">
+          <header className="flex flex-col justify-between space-y-1">
             <time className="flex-shrink-0 text-[#00000033] dark:text-[#FFFFFF33] text-xs md:text-sm font-semibold uppercase">
               {post?.publishDay}
             </time>
-            <h2 className="transition text-xl md:text-2xl font-medium mb-2 cursor-pointer text-[#000000F0] hover:text-[#D0CDC2] dark:text-[#FFFFFFF0] dark:hover:text-gray-400">
+            <h2 className="transition text-base md:text-xl font-medium mb-2 cursor-pointer text-[#000000F0] hover:text-[#D0CDC2] dark:text-[#FFFFFFF0] dark:hover:text-gray-400">
               {post.title}
             </h2>
           </header>
-          <main>
-            <p className="mt-[10px] text-xl md:text-lg text-[#00000094] dark:text-[#FFFFFF94]">
-              {post.summary}
-            </p>
-          </main>
         </div>
       </article>
 
@@ -37,4 +29,4 @@ const BlogPost = ({ post }) => {
   )
 }
 
-export default BlogPost
+export default RecommendBlog
