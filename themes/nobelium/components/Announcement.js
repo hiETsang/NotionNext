647a733b4de69d2140852b6155afb720e1e650fa
canoe <canoe_likecode@163.com>
2024-03-26 11:01:08 +0800

    update

--- themes/nobelium/components/Announcement.js
+++ themes/nobelium/components/Announcement.js
@@ -4,31 +4,6 @@ import { useEffect } from 'react';
 const NotionPage = dynamic(() => import('@/components/NotionPage'))
 
 const Announcement = ({ notice, className }) => {
-  useEffect(() => {
-    if (notice?.blockMap) {
-      const script = document.createElement("script");
-      script.text = `
-        window.CustomSubstackWidget = {
-          substackUrl: "xcanoe.substack.com",
-          placeholder: "example@gmail.com",
-          buttonText: "Subscribe",
-          theme: "custom",
-          colors: {
-            primary: "#FFDD03",
-            input: "#00000000",
-            email: "#8A8A8A",
-            text: "#000000",
-          }
-        };
-      `;
-      document.getElementById("custom-substack-embed").appendChild(script);
-
-      const script2 = document.createElement("script");
-      script2.src = "https://substackapi.com/widget.js";
-      script2.async = true;
-      document.getElementById("custom-substack-embed").appendChild(script2);
-    }
-  }, [notice]);
 
   if (notice?.blockMap) {
     return (
@@ -38,8 +13,6 @@ const Announcement = ({ notice, className }) => {
             <div id="announcement-content">
               <div className="flex flex-col gap-3 justify-center items-center">
                 <div className="my-5 bg-gray-200 dark:bg-gray-600 w-20 h-1"></div>
-                {/* <div className="text-[#000000CC] dark:text-[#ffffffcc] text-[18px]">欢迎订阅我，及时获取一个普通人视角下的独立观察和真诚输出。</div>
-                <div id="custom-substack-embed"></div> */}
               </div>
               <NotionPage post={notice} className='' />
               <div className="flex justify-center items-center">
